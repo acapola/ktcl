4851c1a279654507fd91c9c3896de7a1c0a966b8
acapola <matic@nimp.co.uk>
2014-11-23 01:17:13 +0100

    arg checking added

--- dat_analysis/raw_to_text.cpp
+++ dat_analysis/raw_to_text.cpp
@@ -1,9 +1,50 @@
  #include <tcl.h>
 
- static int 
- raw_to_text_cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
-     Tcl_SetObjResult(interp, Tcl_NewStringObj("Hello, World!", -1));
-     return TCL_OK;
+#define IN_CHAN_IDX 0
+#define OUT_CHAN_IDX 1
+#define MAX_LENGTH_IDX 2
+
+ static int raw_to_text_cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
+	Tcl_Channel input_channel;
+	Tcl_Channel output_channel;
+	int max_length;
+	int mode;
+	
+	if(objc!=3) goto wrongArgs;
+	input_channel = Tcl_GetChannel(interp, Tcl_GetString(objv[IN_CHAN_IDX]), &mode);
+	if (input_channel == (Tcl_Channel) NULL) {
+		return TCL_ERROR;
+	}
+	if ((mode & TCL_READABLE) == 0) {
+		Tcl_AppendResult(interp, "channel \"", Tcl_GetString(objv[IN_CHAN_IDX]),
+			"\" wasn't opened for reading", (char *) NULL);
+		return TCL_ERROR;
+	}
+	
+	output_channel = Tcl_GetChannel(interp, Tcl_GetString(objv[OUT_CHAN_IDX]), &mode);
+	if (output_channel == (Tcl_Channel) NULL) {
+		return TCL_ERROR;
+	}
+	if ((mode & TCL_WRITABLE) == 0) {
+		Tcl_AppendResult(interp, "channel \"", Tcl_GetString(objv[OUT_CHAN_IDX]),
+			"\" wasn't opened for writing", (char *) NULL);
+		return TCL_ERROR;
+	}
+	
+	if (Tcl_GetIntFromObj(interp, objv[MAX_LENGTH_IDX], &max_length) != TCL_OK) {
+		return TCL_ERROR;
+	}
+
+	Tcl_SetObjResult(interp, Tcl_NewStringObj("Hello, World!", -1));
+	return TCL_OK;
+
+wrongArgs:
+    Tcl_AppendResult (interp, "wrong # args: should be:\n",
+	    "  ",
+	    Tcl_GetString(objv[0]),
+	    " file_in file_out ?max_length?",
+	    (char *) NULL);
+    return TCL_ERROR;
  }
 
 int /*DLLEXPORT*/ raw_to_text_init(Tcl_Interp *interp) {
